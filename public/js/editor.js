c9920d697179120ef51a807d27db76c934bab55c
utkarsh <utkarshkumar387@gmail.com>
2021-01-31 12:19:19 +0530

    drag & resize done/utkarsh

--- public/js/editor.js
+++ public/js/editor.js
@@ -63,3 +63,58 @@ function sendCmd(command) {
 function sendCommandArg(command, arg) {
     document.execCommand(command, false, arg);
 }
+
+//draggable and resizable
+interact('.resize')
+    .resizable({
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: true, top: true },
+
+        listeners: {
+            move(event) {
+                var target = event.target
+                var x = (parseFloat(target.getAttribute('data-x')) || 0)
+                var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+                // update the element's style
+                target.style.width = event.rect.width + 'px'
+                target.style.height = event.rect.height + 'px'
+
+                // translate when resizing from top or left edges
+                x += event.deltaRect.left
+                y += event.deltaRect.top
+
+                target.style.webkitTransform = target.style.transform =
+                    'translate(' + x + 'px,' + y + 'px)'
+
+                target.setAttribute('data-x', x)
+                target.setAttribute('data-y', y)
+            }
+        }
+    })
+
+interact('.drag')
+    .draggable({
+        listeners: {
+            // call this function on every dragmove event
+            move: dragMoveListener,
+        }
+    })
+function dragMoveListener(event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)'
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
